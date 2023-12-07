b6397fb1c311165ebe11dd2472dbc3af36f1cbe7
John Burnett <github@johnburnett.com>
2023-12-06 19:57:55 -0800

    wip

--- cloudymusic.py
+++ cloudymusic.py
@@ -1,14 +1,46 @@
 import json
 import os
+import plistlib
+import re
+import sys
 
-from itunesLibrary import library
 import spotipy
 
 
 SPOTIFY_SECRETS_FILE_NAME = 'spotify_secrets.json'
 
+AUDIO_KINDS = {
+    'AAC audio file',
+    'MPEG audio file',
+    'Purchased AAC audio file',
+}
 
-debug = print
+VIDEO_KINDS = {
+    'MPEG-4 video file',
+    'Protected MPEG-4 video file',
+    'Purchased MPEG-4 video file',
+    'QuickTime movie file',
+}
+
+
+def fatal(msg):
+    print(f'ERROR: {msg}')
+    sys.exit(1)
+
+
+def warn(msg):
+    print(f'WARNING: {msg}')
+
+
+info = print
+
+
+def debug(msg):
+    print(f'DEBUG: {msg}')
+
+
+# def debug(msg):
+#     pass
 
 
 def authenticate():
@@ -36,33 +68,42 @@ def authenticate():
     return client
 
 
-def test_itunes():
-    lib_path = os.path.join(os.getenv("USERPROFILE"), "Music/iTunes/iTunes Music Library.xml")
-    lib_path = r"C:\Users\john\Downloads\Library_test.xml"
-    lib = library.parse(lib_path)
+def iter_itunes_tracks(library_path):
+    with open(library_path, mode='rb') as fp:
+        library = plistlib.load(fp, fmt=plistlib.FMT_XML)
+    tracks = library.get('Tracks', {})
+    debug(f'Loaded {len(tracks)} tracks from "{library_path}"')
 
-    debug(f'Loaded {len(lib)} items from "{lib_path}"')
-
-    for item in [list(lib)[50]]:
-        if item.getItunesAttribute('Track Type') != 'File':
-            debug(f'skipping {item}, not a file')
+    for track_id, track in sorted(tracks.items()):
+        if track.get('Track Type') != 'File':
+            debug(f'skipping track {track_id}, not a file')
             continue
-        if item.getItunesAttribute('Kind') != 'AAC audio file':
-            debug(f'skipping {item}, not an AAC audio file')
+        kind = track.get('Kind')
+        if kind in VIDEO_KINDS:
+            debug(f'skipping track {track_id}, is a video')
             continue
-        if item.getItunesAttribute('Genre') == 'Podcast':
-            debug(f'skipping {item}, is a podcast')
+        assert kind in AUDIO_KINDS
+        if track.get('Genre') == 'Podcast':
+            debug(f'skipping track {track_id}, is a podcast')
             continue
 
-        title = item.getItunesAttribute('Name')
-        artist = item.getItunesAttribute('Artist')
-        album_artist = item.getItunesAttribute('Album Artist')
-        album = item.getItunesAttribute('Album')
+        yield {
+            'album': track.get('Album'),
+            'album_artist': track.get('Album Artist'),
+            'artist': track.get('Artist'),
+            'rating': track.get('Album Rating'),
+            'title': track.get('Name'),
+        }
 
-        print(f'{title=}')
-        print(f'{artist=}')
-        print(f'{album_artist=}')
-        print(f'{album=}')
+
+def strip_album_suffix(album):
+    match = re.search(r'(?P<album>.*) \(?Dis[ck] \d+\)?', album)
+    if match:
+        return match['album'].rstrip()
+    match = re.search(r'(?P<album>.*) \(?Volume \d+\)?', album)
+    if match:
+        return match['album'].rstrip()
+    return album
 
 
 def test_spotipy(client):
@@ -77,5 +118,51 @@ def test_spotipy(client):
         print(album['name'])
 
 
-client = authenticate()
-test_spotipy(client)
+def test_find_albums():
+    library_path = os.path.join(os.getenv("USERPROFILE"), "Music/iTunes/iTunes Music Library.xml")
+
+    found_album_uris = []
+    missing_albums = []
+
+    client = authenticate()
+    searched_queries = set()
+    for ii, track in enumerate(iter_itunes_tracks(library_path)):
+        album = track['album']
+        simplified_album = strip_album_suffix(album)
+        album_artist = track['album_artist']
+
+        query = f'artist:{album_artist} album:{simplified_album}'
+        if query in searched_queries:
+            continue
+        searched_queries.add(query)
+        result = client.search(q=query, type='album')
+        # with open('c:/t/json.json', 'w') as fp:
+        #     json.dump(result, fp, indent=2)
+        # debug(result)
+
+        items = result.get('albums', {}).get('items')
+        if items:
+            album = items[0]
+            album_uri = album['uri']
+            album_name = album['name']
+            album_artist_uris = [artist['uri'] for artist in album['artists']]
+            album_artist_names = [artist['name'] for artist in album['artists']]
+            info(f'Found album: {album_name=} ({album_artist_names=})')
+            found_album_uris.append(album_uri)
+        else:
+            missing_albums.append({'album': album, 'album_artist': album_artist})
+            warn(f'No album found named "{album}" for artist "{album_artist}"')
+
+        if (len(found_album_uris) + len(missing_albums)) >= 300:
+            break
+
+    data = dict(found_album_uris=found_album_uris, missing_albums=missing_albums)
+    with open('C:/t/json.json', 'w') as fp:
+        json.dump(data, fp, indent=2)
+
+
+if __name__ == '__main__':
+    try:
+        test_find_albums()
+    except KeyboardInterrupt:
+        pass
